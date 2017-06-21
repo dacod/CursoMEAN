54de0f3b75e82b83f0c0486d2af37b734cd54d30
Daian Conrad <daian.conrad@gmail.com>
2017-06-21 12:30:05 -0300

    Finalizado curso sem autenticacao

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://172.17.0.2/db_finance')
+module.exports = mongoose.connect('mongodb://localhost/db_finance')
 
 mongoose.Error.messages.general.required = "O Atributo '{PATH}' é obrigatório!"
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
